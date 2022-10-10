d98d6c797d72229d8adc3a2899f4a75353b21341
ethan-stone <ethan.stone9352@gmail.com>
2022-10-10 00:11:48 -0400

    started working on model concept

--- csfle/seed-db.ts
+++ csfle/seed-db.ts
@@ -1,63 +1,23 @@
-import { UUID, Binary } from "bson";
-import { MongoClient } from "mongodb";
-import {
-  ClientEncryption,
-  ClientEncryptionOptions
-} from "mongodb-client-encryption";
-import { assertEnvVar, loadEnv } from "../helpers/load-env";
+import { loadEnv } from "../helpers/load-env";
 import { Chance } from "chance";
+import { userRepo, User, UserInsert } from "./user-repo";
 
 loadEnv();
 
-const mongurl = assertEnvVar(process.env.DATABASE_URL, "Missing mongo url");
-
-const client = new MongoClient(mongurl);
-
 const chance = new Chance();
 
-type User = {
-  _id: UUID;
-  email: Binary; // email is encrypted thus it should be binary
-};
-
 async function main() {
-  const db = client.db("csfle");
-  const users = db.collection<User>("users");
-
-  const clientEncryptionOptions: ClientEncryptionOptions = {
-    keyVaultNamespace: "encryption.__keyVault",
-    kmsProviders: {
-      gcp: {
-        email: assertEnvVar(process.env.GCP_EMAIL, "Missing GCP_EMAIL"),
-        privateKey: assertEnvVar(
-          process.env.GCP_PRIVATE_KEY,
-          "Missing GCP_PRIVATE_KEY"
-        )
-      }
-    }
-  };
-
-  const encryption = new ClientEncryption(client, clientEncryptionOptions);
-
-  const dataKeyId = "wVaRu37fSkS4XtelFrT4tg==";
-
-  const usersArray: User[] = [];
+  const usersArray: UserInsert[] = [];
   for (let i = 0; i < 100; i++) {
     usersArray.push({
-      _id: new UUID(),
-      email: await encryption.encrypt(chance.email(), {
-        algorithm: "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic",
-        keyId: new Binary(Buffer.from(dataKeyId, "base64"), 4)
-      })
+      email: chance.email()
     });
   }
 
-  await users.insertMany(usersArray);
+  const result = await userRepo.insertMany(usersArray);
 
-  console.log("Users inserted");
-  console.log(usersArray);
-
-  await client.close();
+  console.log("Result\n-----------");
+  console.log(result);
 }
 
 main();
---
+++ csfle/user-repo.ts
@@ -0,0 +1,95 @@
+import { Binary, UUID } from "bson";
+import { Collection, Db, MongoClient } from "mongodb";
+import { ClientEncryption } from "mongodb-client-encryption";
+import { loadEnv, assertEnvVar } from "../helpers/load-env";
+import { randomUUID } from "crypto";
+
+loadEnv();
+
+const mongourl = assertEnvVar(
+  process.env.DATABASE_URL,
+  "Missing DATABASDE_URL"
+);
+
+const client = new MongoClient(mongourl);
+const encryption = new ClientEncryption(client, {
+  keyVaultNamespace: "encryption.__keyVault",
+  kmsProviders: {
+    gcp: {
+      email: assertEnvVar(process.env.GCP_EMAIL, "Missing GCP_EMAIL"),
+      privateKey: assertEnvVar(
+        process.env.GCP_PRIVATE_KEY,
+        "Missing GCP_PRIVATE_KEY"
+      )
+    }
+  }
+});
+
+type MongoUser = {
+  _id: UUID;
+  email: Binary; // encrypted
+};
+
+export type User = {
+  _id: string;
+  email: string;
+};
+
+export type UserInsert = Omit<User, "_id">;
+
+const dataKeyId = "wVaRu37fSkS4XtelFrT4tg==";
+
+class UserRepo {
+  private collection: Collection<MongoUser>;
+
+  constructor(db: Db) {
+    this.collection = db.collection<MongoUser>("users");
+  }
+
+  private async serialize(user: User) {
+    const mongoUser: MongoUser = {
+      _id: new UUID(user._id),
+      email: await encryption.encrypt(user.email, {
+        algorithm: "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic",
+        keyId: new Binary(Buffer.from(dataKeyId, "base64"), 4)
+      })
+    };
+
+    return mongoUser;
+  }
+
+  private async deserialize(mongoUser: MongoUser) {
+    const user: User = {
+      _id: mongoUser._id.toString(),
+      email: await encryption.decrypt(mongoUser.email)
+    };
+
+    return user;
+  }
+
+  public async insertOne(user: UserInsert) {
+    const mongoUser = await this.serialize({ _id: randomUUID(), ...user });
+    const result = await this.collection.insertOne(mongoUser);
+    if (!result.acknowledged) throw new Error("Operation was not acknowledged");
+    return { _id: result.insertedId };
+  }
+
+  public async insertMany(users: Array<UserInsert>) {
+    const mongoUsers = await Promise.all(
+      users.map((user) => {
+        return this.serialize({
+          _id: randomUUID(),
+          ...user
+        });
+      })
+    );
+
+    const result = await this.collection.insertMany(mongoUsers);
+
+    return {
+      acknowledged: result.acknowledged
+    };
+  }
+}
+
+export const userRepo = new UserRepo(client.db("repos"));
